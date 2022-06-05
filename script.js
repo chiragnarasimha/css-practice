57c7a2da19ebf0977a4f8be203ab27aab0bb7759
c.narasimha.murthy <B5t-G5m-M5nster>
2022-06-06 05:08:51 +1000

    Added some temporary code to make the page scroll to the section that i am working on currently
    Added some typography for the secondary heading

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-document.getElementById("bottom").scrollIntoView();
+// This is just temporary code to make the page scroll to the section that i am working on currently
+window.scroll({
+  top: 400,
+  left: 100,
+  behavior: "smooth",
+});
 console.log("test");
