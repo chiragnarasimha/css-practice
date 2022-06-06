f5ea550221062bebea9fb2f436d2a56147027c8f
c.narasimha.murthy <B5t-G5m-M5nster>
2022-06-06 11:14:42 +1000

    Added Styling for the card picture

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Create custom css shapes to be allied to an image
 
 linea -> https://linea.io/
 Get free icons
+
+unsplash -> https://unsplash.com/
+Free Images to use for the website
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // This is just temporary code to make the page scroll to the section that i am working on currently
 window.scroll({
-  top: 4000,
+  top: 2500,
   left: 100,
   behavior: "smooth",
 });
