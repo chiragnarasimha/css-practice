ad8dd464a9ba1e4b5e8d699668bf94fe60b09705
c.narasimha.murthy <B5t-G5m-M5nster>
2022-06-10 08:53:25 +1000

    Removed the code that will auto scroll the page

--- script.js
+++
@@ -1,7 +0,0 @@
-// This is just temporary code to make the page scroll to the section that i am working on currently
-window.scroll({
-  top: 3500,
-  left: 100,
-  behavior: "smooth",
-});
-console.log("test");
